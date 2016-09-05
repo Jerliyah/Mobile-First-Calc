b54e20cd7759b809a3732a17aff4c21f03cb8a5b
Jerliyah <jerliyahcraig@gmail.com>
2016-09-05 12:21:11 -0500

    HTML - added equal sign button.   SCSS - styled equal sign button, different from other operations.   JS - cleaner and smarter functions. Successful adding button values to calc array for later calculation

--- js/action.js
+++ js/action.js
@@ -10,15 +10,12 @@ function grab(idName) {
 var btn_1 = grab('one');
 var btn_2 = grab('two');
 var btn_3 = grab('three');
-
 var btn_4 = grab('four');
 var btn_5 = grab('five');
 var btn_6 = grab('six');
-
 var btn_7 = grab('seven');
 var btn_8 = grab('eight');
 var btn_9 = grab('nine');
-
 var btn_0 = grab('zero');
 
 // Create variables for operation butttons
@@ -26,9 +23,16 @@ var plus = grab('plus');
 var minus = grab('minus');
 var times = grab('times');
 var divided = grab('divided');
+var equals = grab('equals');
 
 // Create variable for calc screen
 var screen = grab('screen');
+
+
+
+/* Variables */
+var calc = [];
+var btn_value = '';
 
 
 
@@ -46,28 +50,65 @@ click(plus);
 click(minus);
 click(times);
 click(divided);
+click(equals);
 
 
 
 
 /* Main Functions */
 function click(btn) {
-    btn.addEventListener( 'click',  function() { add_to_screen(btn) } );
+    // Calculate values on screen if equal sign pressed
+    if ( btn.id === 'equals' ) {
+        btn.addEventListener( 'click',  function() { calculate() } );
+    }
+    // Simply add values to screen if numbers or operators
+    else {
+        btn.addEventListener( 'click',  function() { add_to_screen(btn) } );
+    }
 }
 
-function add_to_screen(btn) {
-    // If btn is operation, put space around it on the calc screen
-    if ( btn.classList.contains('opr') ) {
-        var no_space_html = btn.innerHTML.toString().trim();
-        var with_space_html = no_space_html.replace(/\s/g, '&nbsp;&nbsp;')
 
-        screen.innerHTML += ' ' + with_space_html + ' ';
+function add_to_screen(btn) {
+    // Trim HTML because outside space and line breaks in markup will appear
+    btn.innerHTML = btn.innerHTML.trim();
+
+    // If btn is an operation, put space around its symbol for calc screen
+    if ( btn.classList.contains('opr') ) {
+        var added_space_html = btn.innerHTML.replace(/\s/g, '&nbsp;&nbsp;');
+        screen.innerHTML += ' ' + added_space_html + ' ';
     }
     // Otherwise, no spacing
     else {
         screen.innerHTML += btn.innerHTML;
     }
 
+    add_to_calc(btn);
+}
+
+
+function add_to_calc(btn) {
+    // Strip btn html to its value
+    // HTML Format:<h2> ? </h2>
+    var btn_value = btn.innerHTML.slice(5,6);
+
+    // Add that value to calc array for later calculation
+    // Convert strings to numbers
+    if( parseInt(btn_value) ) {
+        var btn_num = parseInt(btn_value);
+        calc.push(btn_num);
+    }
+    // If they can't be converted (the operations), just push to calc as string of symbol
+    else {
+        calc.push(btn_value);
+    }
+
+    console.log(calc);
+}
+
+
+function calculate() {
+    var mathInput = screen.innerHTML;
+    console.log(mathInput);
 }
 
 
